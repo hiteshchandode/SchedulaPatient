210765529cd77a96c247b0eabc2e2a9b8e1289fb
Hitesh chandode <hiteshchandode24@gmail.com>
2026-03-13 22:47:16 +0530

    updated feature and fixed styling

--- app/src/main/java/com/example/schedulapatientapp/AppointmentDetailsScreen.kt
+++ app/src/main/java/com/example/schedulapatientapp/AppointmentDetailsScreen.kt
@@ -164,7 +164,7 @@ fun AppointmentDetailsScreen(navController: NavController) {
             Button(
                 onClick = {
                     // GO TO NEXT SCREEN
-                    navController.navigate("payment_method")
+                    navController.navigate("payment _method")
                 },
                 modifier = Modifier.fillMaxWidth().height(50.dp),
                 shape = RoundedCornerShape(8.dp),
